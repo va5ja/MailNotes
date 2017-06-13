24c9666f2c4de43aa0e364af224a3f15346ded35
Vasja Laharnar <vl@plista.com>
2017-06-13 10:50:05 +0200

    Add CLI functionality

--- src/AppBundle/Command/AppParseMailCommand.php
+++ src/AppBundle/Command/AppParseMailCommand.php
@@ -2,11 +2,14 @@
 
 namespace AppBundle\Command;
 
+use AppBundle\Entity\Category;
+use AppBundle\Entity\Note;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Debug\Exception\ContextErrorException;
 
 class AppParseMailCommand extends ContainerAwareCommand
 {
@@ -15,67 +18,101 @@ class AppParseMailCommand extends ContainerAwareCommand
         $this
             ->setName('mail-notes:fetch')
             ->setDescription('MailNotes mail fetcher')
-            ->addArgument('argument', InputArgument::OPTIONAL, 'Argument description')
-            ->addOption('option', null, InputOption::VALUE_NONE, 'Option description');
+            ->addOption('method', 'm', InputOption::VALUE_OPTIONAL, 'Fetch method');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $argument = $input->getArgument('argument');
+        $method = $input->getOption('method') ? $input->getOption('method') : 'email-piping';
+        $outputMessage = '';
 
-        if ($input->getOption('option')) {
-            // ...
+        switch ($method) {
+            case 'email-piping':
+                $fd = fopen('php://stdin', 'r');
+                $rawEmailContent = "";
+                while (!feof($fd)) {
+                    $rawEmailContent .= fread($fd, 1024);
+                }
+                fclose($fd);
+
+                $importEmail = $this->importEmail($rawEmailContent);
+                if ($importEmail === true) {
+                    $outputMessage = 'Successfully fetched one email.';
+                } else {
+                    $outputMessage = 'Error occured while fetching message: ' . $importEmail;
+                }
+
+                break;
+            case 'imap':
+                $hostname = $this->getContainer()->getParameter('imap_hostname');
+                $username = $this->getContainer()->getParameter('imap_username');
+                $password = $this->getContainer()->getParameter('imap_password');
+
+                try {
+                    $inbox = imap_open($hostname, $username, $password);
+                    $emails = imap_search($inbox, 'ALL');
+
+                    if ($emails) {
+                        $outputMessage = [];
+                        $fetchedEmails = 0;
+                        foreach ($emails as $email_number) {
+                            $rawEmailContent = imap_fetchbody($inbox, $email_number, '');
+                            $importEmail = $this->importEmail($rawEmailContent);
+                            if ($importEmail === true) {
+                                $fetchedEmails += 1;
+                            } else {
+                                $outputMessage[] = "Error occured while fetching message $email_number: $importEmail";
+                            }
+                        }
+                        $outputMessage[] = "Successfully fetched $fetchedEmails email(s).";
+                    }
+
+                    imap_close($inbox);
+                } catch (ContextErrorException $e) {
+                    $outputMessage = $e->getMessage();
+                }
+                break;
         }
 
-        $fd = fopen("php://stdin", "r");
-        $email = "";
-        while (!feof($fd)) {
-            $email .= fread($fd, 1024);
-        }
-        fclose($fd);
+        $output->writeln($outputMessage);
+    }
 
+    /**
+     * @param string $rawEmailContent
+     * @return bool
+     */
+    protected function importEmail($rawEmailContent)
+    {
+        $em = $this->getContainer()->get('doctrine')->getEntityManager();
         $mailParser = new \ZBateson\MailMimeParser\MailMimeParser();
-        $message = $mailParser->parse($email);
-        $category = ucfirst($message->getHeaderValue('subject'));
+        $message = $mailParser->parse($rawEmailContent);
+        $categoryName = ucfirst($message->getHeaderValue('subject'));
         $content = array_map('trim', explode("\n", $message->getTextContent()));
         $password = array_shift($content);
         $content = implode("\n", array_filter($content));
+        $return = true;
 
         if ($password === date('d') . '123') {
-            $dsn = 'mysql:host=localhost;dbname=laharnar_notes;charset=utf8';
-            $user = 'laharnar_nts';
-            $password = 'qV$ps_-lU_24';
-
-            try {
-                $dbh = new \PDO($dsn, $user, $password);
-
-                $stmt = $dbh->prepare('SELECT * FROM category WHERE name = ?');
-                if ($stmt->execute(array($category))) {
-                    $row = $stmt->fetch();
-                    if ($row) {
-                        $category_id = $row['id'];
-                    } else {
-                        $stmt = $dbh->prepare('INSERT INTO category (name, datetime) VALUES (:name, :datetime)');
-                        $stmt->bindParam(':name', $category);
-                        $stmt->bindParam(':datetime', date('Y-m-d H:i:s'));
-                        $stmt->execute();
-                        $category_id = $dbh->lastInsertId();
-                    }
-                }
-
-                $stmt = $dbh->prepare('INSERT INTO note (category_id, content, datetime) VALUES (:category_id, :content, :datetime)');
-                $stmt->bindParam(':category_id', $category_id);
-                $stmt->bindParam(':content', $content);
-                $stmt->bindParam(':datetime', date('Y-m-d H:i:s'));
-                $stmt->execute();
-            } catch (\PDOException $e) {
-                echo 'Connection failed: ' . $e->getMessage();
+            $category = $em->getRepository('AppBundle:Category')->findBy(['name' => $categoryName]);
+            if (!$category) {
+                $category = new Category();
+                $category->setName($categoryName);
+                $category->setDatetime(new \DateTime());
+                $em->persist($category);
+                $em->flush();
             }
+
+            $note = new Note();
+            $note->setCategory($category);
+            $note->setContent($content);
+            $note->setDatetime(new \DateTime());
+            $em->persist($note);
+            $em->flush();
         } else {
-            echo 'Wrong token.';
+            $return = 'Wrong token.';
         }
 
-        $output->writeln('Command result.');
+        return $return;
     }
 
 }
