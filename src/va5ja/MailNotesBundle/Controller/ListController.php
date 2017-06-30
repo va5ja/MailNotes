b1cf8967b4cd12e4362c58be9c5f93f7fafb658e
Vasja Laharnar <vl@plista.com>
2017-06-30 17:04:49 +0200

    Fix typo, add form errors

--- src/va5ja/MailNotesBundle/Controller/ListController.php
+++ src/va5ja/MailNotesBundle/Controller/ListController.php
@@ -15,7 +15,7 @@ class ListController extends Controller
         $notes = $noteRepository->findAll();
 
         return $this->render(
-            'List/List.'.$_format.'.twig',
+            'List/list.'.$_format.'.twig',
             [
                 'categoryid' => false,
                 'notes' => $notes,
@@ -32,7 +32,7 @@ class ListController extends Controller
         $notes = $noteRepository->findby(['category' => $category]);
 
         return $this->render(
-            'List/List.'.$_format.'.twig',
+            'List/list.'.$_format.'.twig',
             [
                 'categoryid' => $category->getId(),
                 'notes' => $notes,
